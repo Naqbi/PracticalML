2eef7d006b07d23a75249b336674ca86fd8c4458
Ahmed Al Naqbi <aqusoom@hotmail.com>
2021-10-02 00:20:16 +0400

    Change where the answers saved.

--- Assignment.R
+++ Assignment.R
@@ -59,7 +59,7 @@ predictionsB2 <- predict(modFitB1, testing, type = "class")
 pml_write_files = function(x){
   n = length(x)
   for(i in 1:n){
-    filename = paste0("myexp/problem_id_",i,".txt")
+    filename = paste0("problem_id_",i,".txt")
     write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
   }
 }
